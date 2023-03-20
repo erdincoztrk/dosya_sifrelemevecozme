42f5dc66ce37b62643af0da1e73ee3aa27d62ea6
Erdinç Öztürk <e.erdincoztrk@gmail.com>
2023-03-21 00:34:09 +0300

    Add project files.

---
+++ dosyasifre/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dosyasifre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dlg = new OpenFileDialog();
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                label1.Text = dlg.FileName;
+                textBox1.Text = dlg.FileName;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        { try
+            {
+                Byte[] dosya = File.ReadAllBytes(label1.Text);
+                for (int i = 0; i < dosya.Length; i++)
+                {
+                    dosya[i] = (Byte)((int)dosya[i] + 1);
+                    if (dosya[i] > 255)
+                    {
+                        dosya[i] = 0;
+                    }
+                }
+
+                File.WriteAllBytes(textBox1.Text, dosya);
+
+                MessageBox.Show("ŞİFRELEME BAŞARILI");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("ŞİFRELEME BAŞARISIZ");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Byte[] dosya = File.ReadAllBytes(label1.Text);
+                for (int i = 0; i < dosya.Length; i++)
+                {
+
+                    if ((Byte)((int)dosya[i] - 1) < 0)
+                    {
+                        dosya[i] = 255;
+                    }
+                    else
+                    {
+                        dosya[i] = (Byte)((int)dosya[i] - 1);
+                    }
+                }
+
+                File.WriteAllBytes(textBox1.Text, dosya);
+
+                MessageBox.Show("ŞİFRE ÇÖZÜLDÜ");
+            }
+            catch (Exception)
+            {
+
+                MessageBox.Show("HATA");
+            }
+        }
+
+       
+        
+        public void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        
+    }
+}
---
+++ dosyasifre/SifreGiris.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dosyasifre
+{
+    public partial class SifreGiris : Form
+    {
+        public SifreGiris()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.UseSystemPasswordChar == true)
+            {
+                textBox1.UseSystemPasswordChar = false;
+            }
+            else
+            {
+                textBox1.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sifre = "Yalova771907...";
+            if(textBox1.Text == sifre)
+            {
+                Form1 frm = new Form1();
+                MessageBox.Show("GİRİŞ BAŞARILI", "YÖNLENDİRİLİYOR", MessageBoxButtons.OK , MessageBoxIcon.Information);
+                frm.Show(); 
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("GİRİŞ BAŞARSIZI", "YENİDEN DENE", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                textBox1.Clear();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
